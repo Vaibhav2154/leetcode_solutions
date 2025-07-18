a770f01c9d214abfbbab0d1871a03809ffda8fb8
Vaibhav M N <vaibhavvaibhu2005@gmail.com>
2025-07-18 19:13:21 +0000

    Added first one

---
+++ hashing/1.py
@@ -0,0 +1,9 @@
+    class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mpp = {}
+        for i, num in enumerate(nums):
+            num2 = target - num
+            if num2 in mpp:
+                return [mpp[num2],i]
+            mpp[num] = i
+        return []
