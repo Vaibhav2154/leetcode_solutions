715e2d6222efbd413e782fd23122bc4ad4965eb0
Vaibhav M N <vaibhavvaibhu2005@gmail.com>
2026-02-18 09:50:24 +0530

    Brute force for problem 693

---
+++ bit_manipulation/693.py
@@ -0,0 +1,10 @@
+# Brute force
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        n = bin(n)
+        n = n[2:]
+        for i in range(1,len(n)):
+            if n[i-1] == n[i]:
+                return False
+        
+        return True
